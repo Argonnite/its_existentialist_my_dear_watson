0333d6c734177399a17c2fb7cba0f189d2c9cd4f
Cole Willsea <coleww@gmail.com>
2014-11-05 22:55:20 -0800

    special saving mod

--- existential_detective.rb
+++ existential_detective.rb
@@ -9,7 +9,7 @@ end
 put_downs = ["a lack of respect.",  "an air of condescension.", "a hint of pomp and circumstance.", "a dreary sigh...", "an uncomfortable level of candor.", "a frightening level of disdain for the working class.", "an addendum that we should all recycle and drink more water.", "an urgent plea to stop global warming.", "a sort of gurgling noise that sounded like 'akdjfniuiuuiiuuiuiiiiii'", "an indiscernible level of smugness", "their ring tone going off at a very un ignorable volume level." ]
 
 novel = "On a cold and dark night in London, Sherlock Holmes and Dr. Watson are out and about solving crimes...\n\n" +
-"THE TALE OF THE " + mark.generate.upcase + " VOLUME: " + ((rand 12) + 1).to_s + "\n"
+(title = "THE TALE OF THE " + mark.generate.upcase) + " VOLUME: " + ((rand 12) + 1).to_s + "\n"
 
 until novel.split.length > 50000
   novel << " \"" + mark.generate + (rand < 0.2 ? mark.generate : '' ) + "\"" + ' said watson to holmes with ' + put_downs.sample + "\n"
@@ -17,4 +17,6 @@ until novel.split.length > 50000
   novel << " \"" + mark.generate + (rand < 0.2 ? mark.generate : '' ) + "\"" + ' said holmes to watson with ' + put_downs.sample + "\n"
 end
 
-puts novel
+File.open('text.txt', 'w') do |f|
+  f.puts novel
+end
