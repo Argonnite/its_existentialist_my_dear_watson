1ef6af455b1d4ffb783dabd7598ac986429ec0eb
Cole Willsea <coleww@gmail.com>
2014-11-06 08:49:15 -0800

    progressively decreasing ngram

--- existential_detective.rb
+++ existential_detective.rb
@@ -1,6 +1,6 @@
 require 'mrkv'
 
-mark = Mrkv::Chain.new 3
+mark = Mrkv::Chain.new 4
 
 Dir.entries('sauce')[2..-1].each do |file|
   mark.add File.readlines 'sauce/' + file
@@ -29,14 +29,81 @@ put_downs = ["a lack of respect.",
 novel = "<div style='width: 640px; margin: auto;font-family: Arial, Helvetica, sans-serif;'><h2><em>On a cold and dark night in London, Sherlock Holmes and Dr. Watson are having a passionate discussion about an ongoing case...</em></h2>\n\n" +
 (title = "<h1><strong>THE CASE OF THE " + mark.generate.upcase) + " VOLUME: " + ((rand 12) + 1).to_s + "</strong></h1>\n\n\n"
 
+def an_aside mark
+  dudes = %w(Watson Holmes)
+  actions = ["stares out at the",
+            "paces back and forth amid the",
+            "rudely answers a phone call from the",
+            "orders takeout from the",
+            "grimly considers the magnitude of the",
+    ]
+
+  stuffz = ["night sky", "chaos", "eternal darkness", "law enforcement", "pizza place"]
+
+  talk_verbs = ["thinks to himself: </sub><strong>\"",
+                "wonders aloud: </sub><strong>\"",
+                "murmurs under his breath: </sub><strong>\"",
+                "sighs as he considers: </sub><strong>\""]
+
+  "<em><sub>"+dudes.sample+" "+actions.sample+" "+stuffz.sample+" and "+talk_verbs.sample+mark.generate[0...-1]+"?\"</strong></em>"
+end
+
+
+
+until novel.split.length > 12000
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Dr. Watson to Inspector Holmes with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Inspector Holmes to Dr. Watson with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << (an_aside mark) if rand < 0.1
+end
+
+mark = Mrkv::Chain.new 3
+
+Dir.entries('sauce')[2..-1].each do |file|
+  mark.add File.readlines 'sauce/' + file
+end
+
+until novel.split.length > 20000
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Dr. Watson to Inspector Holmes with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Inspector Holmes to Dr. Watson with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << (an_aside mark) if rand < 0.1
+end
+
+mark = Mrkv::Chain.new 2
+
+Dir.entries('sauce')[2..-1].each do |file|
+  mark.add File.readlines 'sauce/' + file
+end
+
+
+until novel.split.length > 32000
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Dr. Watson to Inspector Holmes with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Inspector Holmes to Dr. Watson with ' + put_downs.sample + "</em></sub></p>\n"
+
+  novel << (an_aside mark) if rand < 0.1
+end
+
+mark = Mrkv::Chain.new 1
+
+Dir.entries('sauce')[2..-1].each do |file|
+  mark.add File.readlines 'sauce/' + file
+end
+
 until novel.split.length > 50000
   novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Dr. Watson to Inspector Holmes with ' + put_downs.sample + "</em></sub></p>\n"
 
   novel << "<p>\"" + mark.generate + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.3 ? ' ' + mark.generate : '' ) + (rand < 0.15 ? ' ' + mark.generate : '' )  + "\"<sub><em>" + ' said Inspector Holmes to Dr. Watson with ' + put_downs.sample + "</em></sub></p>\n"
 
+  novel << (an_aside mark) if rand < 0.1
 end
 
-  novel << "<h3><strong><em>THE END</em></strong></h3></div>"
+
+
+novel << "<marquee><h3><strong><em>THE END</em></strong></h3></div></marquee>"
 
 File.open('text.html', 'w') do |f|
   f.puts novel
